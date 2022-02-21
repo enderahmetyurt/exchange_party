a961bbec8646f313b8966097f813d4f117e01453
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-02-21 17:30:52 +0300

    Remove webmock code duplication

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,6 @@ require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = File.expand_path("cassettes", __dir__)
   config.hook_into :webmock
-  config.hook_into :webmock
   config.ignore_localhost = true
   config.allow_http_connections_when_no_cassette = false
 end
